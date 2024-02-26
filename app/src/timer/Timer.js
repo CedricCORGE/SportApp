b03caf1fa9f8de9804d8b06730295dd2c57657ec
Cédric <cedric.corge@epitech.eu>
2024-02-26 17:59:39 +0100

    [ADD] Designed version of timer with sound

--- app/src/timer/Timer.js
+++ app/src/timer/Timer.js
@@ -1,14 +1,132 @@
 import { Time } from "./component/Time";
-import { View, Text } from "react-native";
+import { View, Text, Button, TouchableOpacity } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useState, useEffect } from "react";
+import { Icon, IconButton } from "react-native-paper";
+import { Audio } from "expo-av";
 
 export const Timer = (props) => {
   const { top } = useSafeAreaInsets();
 
+  const [time, setTime] = useState(props.time);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const [sound, setSound] = useState();
+
+  useEffect(() => {
+    if (time === 4 && isRunning) {
+      playSound();
+    }
+    if (time === 0) {
+      stopTimer();
+    }
+  }, [time]);
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../../assets/sounds/timer.wav")
+    );
+    setSound(sound);
+    await sound.playAsync();
+  }
+
+  const startTimer = () => {
+    setIsRunning(true);
+    this.interval = setInterval(() => {
+      setTime((time) => (time === 0 ? 0 : time - 1));
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+    clearInterval(this.interval);
+  };
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    clearInterval(this.interval);
+    setTime(props.time);
+  };
+
   return (
-    <View style={{ marginTop: top }}>
-      <Text>Timer</Text>
-      <Time time={props.time} />
+    <View style={[styles.timerContainer, { marginTop: top }]}>
+      <View
+        style={{ height: "10%", display: "flex", justifyContent: "center" }}
+      >
+        <Text style={{ fontSize: 30 }}>Timer</Text>
+      </View>
+
+      <View style={[styles.time]}>
+        <Time time={time} />
+      </View>
+
+      <View style={[styles.actionButtons]}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            width: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              height: "50%",
+              width: "100%",
+              justifyContent: "center",
+            }}
+          >
+            <TouchableOpacity style={[styles.button]} onPress={startTimer}>
+              <Text style={[styles.buttonText]}>Start</Text>
+              <Icon source="play" size={20} />
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button]} onPress={stopTimer}>
+              <Text style={[styles.buttonText]}>Stop</Text>
+              <Icon source="stop" size={20} />
+            </TouchableOpacity>
+          </View>
+          <IconButton size={30} icon={"restart"} onPress={resetTimer} />
+        </View>
+      </View>
     </View>
   );
+};
+
+const styles = {
+  timerContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  time: {
+    height: "70%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  actionButtons: {
+    display: "flex",
+    flexDirection: "row",
+    color: "black",
+    height: "20%",
+  },
+  button: {
+    backgroundColor: "#E5EB0E",
+    width: "35%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 25,
+    margin: 5,
+  },
+  buttonText: {
+    paddingRight: 5,
+    textTransform: "uppercase",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 };
--- app/src/timer/component/Time.js
+++ app/src/timer/component/Time.js
@@ -1,6 +1,4 @@
 import { View, Text } from "react-native";
-import { useState, useEffect } from "react";
-import { Button } from "react-native-paper";
 
 const getMinutes = (time) => {
   var minutes = Math.floor(time / 60);
@@ -20,45 +18,24 @@ const getSeconds = (time) => {
 };
 
 export const Time = (props) => {
-  const [time, setTime] = useState(props.time);
-  const [isRunning, setIsRunning] = useState(false);
-
-  useEffect(() => {
-    console.log("Time: ", time);
-    if (time === 0) {
-      console.log("Time is up");
-      stopTimer();
-    }
-  }, [time]);
-
-  const startTimer = () => {
-    setIsRunning(true);
-    this.interval = setInterval(() => {
-      setTime((time) => (time === 0 ? 0 : time - 1));
-    }, 1000);
-  };
-
-  const stopTimer = () => {
-    setIsRunning(false);
-    clearInterval(this.interval);
-  };
-
-  const resetTimer = () => {
-    setIsRunning(false);
-    clearInterval(this.interval);
-    setTime(props.time);
-  };
-
   return (
     <View>
-      <Text>
-        {getMinutes(time)}:{getSeconds(time)}
-      </Text>
-      <Button onPress={startTimer} disabled={isRunning}>
-        Start
-      </Button>
-      <Button onPress={stopTimer}>Stop</Button>
-      <Button onPress={resetTimer}>Reset</Button>
+      <View style={[styles.timerContainer]}>
+        <Text style={[styles.timer]}>
+          {getMinutes(props.time)}:{getSeconds(props.time)}
+        </Text>
+      </View>
     </View>
   );
+};
+
+const styles = {
+  timerContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  timer: {
+    fontSize: 70,
+  },
 };
