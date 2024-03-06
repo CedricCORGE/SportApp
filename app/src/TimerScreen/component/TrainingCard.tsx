8c2c832416f7a068f6194f668fa146ce708a3a5b
Cédric <cedric.corge@epitech.eu>
2024-03-06 21:49:56 +0100

    [FIX] migrate js to tsx

--- app/src/TimerScreen/component/TrainingCard.tsx
+++ app/src/TimerScreen/component/TrainingCard.tsx
@@ -1,12 +1,11 @@
-import { Text, TouchableOpacity, View } from "react-native";
-import { Card, Modal } from "react-native-paper";
-import { buttons, layers, texts } from "../../style/globalStyle";
-import { useState } from "react";
-import { DataRow } from "./dataRow";
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Card, Modal} from 'react-native-paper';
+import {buttons, layers, texts} from '../../style/globalStyle';
+import React from 'react';
 
-export const TrainingCard = ({ item }) => {
+export const TrainingCard = ({item}: any) => {
   return (
-    <View style={{ marginBottom: "5%" }}>
+    <View style={{marginBottom: '5%'}}>
       <Card style={[styles.title]}>
         <Card.Content>
           <View>
@@ -15,39 +14,37 @@ export const TrainingCard = ({ item }) => {
               <View style={[styles.trainingData]}>
                 <Text>Repetitions: {item.repetitions}</Text>
                 <Text>
-                  Work:{" "}
+                  Work:{' '}
                   {(item.work.minutes < 10
-                    ? "0" + item.work.minutes
+                    ? '0' + item.work.minutes
                     : item.work.minutes) +
-                    ":" +
+                    ':' +
                     (item.work.seconds < 10
-                      ? "0" + item.work.seconds
+                      ? '0' + item.work.seconds
                       : item.work.seconds)}
                 </Text>
                 <Text>
-                  Rest:{" "}
+                  Rest:{' '}
                   {(item.rest.minutes < 10
-                    ? "0" + item.rest.minutes
+                    ? '0' + item.rest.minutes
                     : item.rest.minutes) +
-                    ":" +
+                    ':' +
                     (item.rest.seconds < 10
-                      ? "0" + item.rest.seconds
+                      ? '0' + item.rest.seconds
                       : item.rest.seconds)}
                 </Text>
               </View>
 
               <View style={[layers.centered]}>
                 <TouchableOpacity
-                  style={[buttons.button, { width: "60%", height: 30 }]}
-                >
+                  style={[buttons.button, {width: '60%', height: 30}]}>
                   <Text style={[texts.s]}>START</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
-                  style={[buttons.button, { width: "60%", height: 30 }]}
+                  style={[buttons.button, {width: '60%', height: 30}]}
                   onPress={() => {
                     item.edit(item);
-                  }}
-                >
+                  }}>
                   <Text style={[texts.s]}>EDIT</Text>
                 </TouchableOpacity>
               </View>
@@ -59,22 +56,22 @@ export const TrainingCard = ({ item }) => {
   );
 };
 
-const styles = {
+const styles = StyleSheet.create({
   title: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "flex-start",
-    backgroundColor: "white",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: 'white',
   },
   trainingData: {
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
   },
   cardContent: {
-    display: "flex",
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
-};
+});
